6b9368afa3e2c54713630556cd16ae688cd3a8d2
MaxJan2010 <33513701+MaxJan2010@users.noreply.github.com>
2023-12-22 14:32:00 +0200

    - Re-Factor The Code

--- src/work/orders/get_open_positions.py
+++ src/work/orders/get_open_positions.py
@@ -4,8 +4,8 @@ import pandas as pd
 # get open positions on Symbol
 def get_open_positions_pair(symbol):
     positions = mt5.positions_get(symbol=symbol)
-    if positions == None:
-        print(f"No positions on " + symbol + "error code={}".format(mt5.last_error()))
+    if not positions:
+        print(f"No positions on " + symbol + " & error code={}".format(mt5.last_error()))
     elif len(positions) > 0:
         print("Total positions on " + symbol + " = " + str(len(positions)))
         # display all open positions
@@ -13,13 +13,14 @@ def get_open_positions_pair(symbol):
             print(position)
 
 # get the list of positions on symbols whose names contain "*USD*"
-usd_positions = mt5.positions_get(group="*USD*")
-if usd_positions == None:
-    print("No positions with group=\"*USD*\", error code={}".format(mt5.last_error()))
-elif len(usd_positions) > 0:
-    print("positions_get(group=\"*USD*\")={}".format(len(usd_positions)))
-    # display these positions as a table using pandas.DataFrame
-    df = pd.DataFrame(list(usd_positions), columns=usd_positions[0]._asdict().keys())
-    df['time'] = pd.to_datetime(df['time'], unit='s')
-    df.drop(['time_update', 'time_msc', 'time_update_msc', 'external_id'], axis=1, inplace=True)
-    print(df)
+def get_open_positions_currency(currency):
+    currency_positions = mt5.positions_get(group=currency)
+    if not currency_positions:
+        print("No positions with group = " + currency + " & error code={}".format(mt5.last_error()))
+    elif len(currency_positions) > 0:
+        print("positions_get group = " + currency+ ") = {}".format(len(currency_positions)))
+        # display these positions as a table using pandas.DataFrame
+        df = pd.DataFrame(list(currency_positions), columns=currency_positions[0]._asdict().keys())
+        df['time'] = pd.to_datetime(df['time'], unit='s')
+        df.drop(['time_update', 'time_msc', 'time_update_msc', 'external_id'], axis=1, inplace=True)
+        print(df)
--- src/work/run.py
+++ src/work/run.py
@@ -14,7 +14,6 @@ mt5.login(login=login, password=password, server=server)
 
 # buy_limit("EURUSD", 0.01, 1.0650)
 # sell_limit("EURUSD", 0.01, 1.0950)
-market_order("USDCHF", 0.01, 'buy')
-get_open_positions_pair('USDCHF')
-
-
+# market_order("USDCHF", 0.01, 'buy')
+# get_open_positions_pair('EURUSD')
+# get_open_positions_currency("*CAD*")
