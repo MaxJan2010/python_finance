e5fc3dfb1401963ebc49996aadb24db81d377a65
MaxJan2010 <33513701+MaxJan2010@users.noreply.github.com>
2023-12-22 18:43:04 +0200

    - Completed Tutorial Bernard Ugochukwu - Tutorial: Build AI FOREX Trading Bots for CRYPTOCURRENY in 3Hours 2023

--- src/work/orders/get_open_positions.py
+++ src/work/orders/get_open_positions.py
@@ -1,8 +1,8 @@
 import MetaTrader5 as mt5
 import pandas as pd
 
-# get open positions on pair
-def get_open_positions_pair(symbol):
+# get open positions on symbol
+def get_open_positions_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     if not positions:
         print(f"No positions on " + symbol + " & error code={}".format(mt5.last_error()))
@@ -12,7 +12,7 @@ def get_open_positions_pair(symbol):
         for position in positions:
             print(position)
 
-# get the list of positions on pair whose names contain "*USD*"
+# get the list of positions on symbol whose names contain "*USD*"
 def get_open_positions_currency(currency):
     currency_positions = mt5.positions_get(group=currency)
     if not currency_positions:
@@ -26,7 +26,7 @@ def get_open_positions_currency(currency):
         print(df)
 
 
-def calc_total_profit_pair(symbol):
+def calc_total_profit_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     df = pd.DataFrame(list(positions), columns=positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
@@ -35,7 +35,7 @@ def calc_total_profit_pair(symbol):
     print("Total Profit of " + symbol + " is = " + str(profit))
     return profit
 
-def calc_total_buy_profit_pair(symbol):
+def calc_total_buy_profit_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     df = pd.DataFrame(list(positions), columns=positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
@@ -45,7 +45,7 @@ def calc_total_buy_profit_pair(symbol):
     print("Total BUY Profit of " + symbol + " is = " + str(profit))
     return profit
 
-def calc_total_sell_profit_pair(symbol):
+def calc_total_sell_profit_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     df = pd.DataFrame(list(positions), columns=positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
@@ -55,7 +55,7 @@ def calc_total_sell_profit_pair(symbol):
     print("Total SELL Profit of " + symbol + " is = " + str(profit))
     return profit
 
-def calc_total_volume_pair(symbol):
+def calc_total_volume_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     df = pd.DataFrame(list(positions), columns=positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
@@ -64,7 +64,7 @@ def calc_total_volume_pair(symbol):
     print("Total Volume of " + symbol + " is = " + str(volume))
     return volume
 
-def calc_total_buy_margin_pair(symbol):
+def calc_total_buy_margin_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     df = pd.DataFrame(list(positions), columns=positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
@@ -81,7 +81,7 @@ def calc_total_buy_margin_pair(symbol):
     print("Total BUY Margin of " + symbol + " is = " + str(sum))
     return sum
 
-def calc_total_sell_margin_pair(symbol):
+def calc_total_sell_margin_symbol(symbol):
     positions = mt5.positions_get(symbol=symbol)
     df = pd.DataFrame(list(positions), columns=positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
@@ -98,12 +98,57 @@ def calc_total_sell_margin_pair(symbol):
     print("Total SELL Margin of " + symbol + " is = " + str(sum))
     return sum
 
-def calc_total_percentage_profit_pair(symbol):
-    total_profit_pair = calc_total_profit_pair(symbol)
-    buy_margin = calc_total_buy_margin_pair(symbol)
-    sell_margin = calc_total_sell_margin_pair(symbol)
+def calc_total_percentage_profit_symbol(symbol):
+    total_profit_symbol = calc_total_profit_symbol(symbol)
+    buy_margin = calc_total_buy_margin_symbol(symbol)
+    sell_margin = calc_total_sell_margin_symbol(symbol)
     total_margin = buy_margin + sell_margin
-    percentage_profit = (total_profit_pair / total_margin) * 100
+    percentage_profit = (total_profit_symbol / total_margin) * 100
     print("Total total_margin of " + symbol + " is = " + str(total_margin))
     print("Total percentage_profit of " + symbol + " is = " + str(percentage_profit))
     return percentage_profit
+
+def close_position(position):
+    tick = mt5.symbol_info_tick(position.symbol)
+
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "position": position.ticket,
+        "symbol": position.symbol,
+        "volume": position.volume,
+        "type": mt5.ORDER_TYPE_BUY if position.type == 1 else mt5.ORDER_TYPE_SELL,
+        "price": tick.ask if position.type == 1 else tick.bid,
+        # "sl": price - 100 * point,
+        # "tp": price + 100 * point,
+        "deviation": 20,
+        "magic": 100,
+        "comment": "python script close ",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_FOK,
+    }
+    
+    result = mt5.order_send(request)
+    return result
+
+
+def close_all_positions_symbol(symbol):
+    positions = mt5.positions_get(symbol=symbol)
+    for position in positions:
+        close_position(position)
+    
+    my_positions = mt5.positions_get(symbol=symbol)
+
+    if not my_positions:
+        print(f"No positions on " + symbol + " & error code={}".format(mt5.last_error()))
+        print("Closed All Open Position Successfully for The Symbol = " + symbol)
+    elif len(my_positions) > 0:
+        print("Total positions on " + symbol + " = " + str(len(my_positions)))
+        # display all open positions
+        for my_position in my_positions:
+            print(my_position)
+        print("Closed All Open Position ERROR for The Symbol = " + symbol)
+        print("PLEASE TRY AGAIN")
+
+    
+
+
